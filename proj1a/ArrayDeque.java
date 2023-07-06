4e9c46e995cce309b4a8e81008990822ca9b8303
ycmm52 <47115279+ycmm52@users.noreply.github.com>
2023-07-06 16:45:03 +0800

    Update ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -9,18 +9,18 @@ public class ArrayDeque<T> {
     private final int FACTOR = 2;
 
     public ArrayDeque() {
-        this.size = INI_SIZE;
-        this.items = (T[]) new Object[this.size];
+        this.size = 0;
+        this.items = (T[]) new Object[INI_SIZE];
         this.start = 0;
         this.stop = 0;
     }
 
     public ArrayDeque(T ele) {
-        this.size = INI_SIZE;
-        this.items = (T[]) new Object[this.size];
+        this.size = 1;
+        this.items = (T[]) new Object[INI_SIZE];
         this.items[0] = ele;
         this.start = 0;
-        this.stop = 1;
+        this.stop = 0;
     }
 
     public ArrayDeque(T[] arr) {
@@ -35,43 +35,93 @@ public class ArrayDeque<T> {
             this.items[i] = arr[i];
         }
         this.start = 0;
-        this.stop = this.size;
+        this.stop = this.size - 1;
+    }
+
+    private void reSize() {
+        int newSize = this.items.length * FACTOR;
+        T[] newArray = (T[]) new Object[newSize];
+        // System.arraycopy(this.items, 0, newArray, 0, this.size);
+        for (i=0; i < this.size ; i += 1;) {
+            newArray[i] = this.items.get(i);
+        }
+        this.start = 0;
+        this.stop = this.size - 1;
+        this.size = newSize;
+        this.items = newArray;
     }
 
     public void addFirst(T item) {
+        T[] arr = (T[]) new Object[1];
+        arr[0] = item;
+        this.addFirst(arr);
     }
 
     public void addFirst(T[] arr) {
+        if (this.size == this.items.length) {
+            this.reSize();
+        }
+        for (i = 0; i < arr.length; i += 1;) {
+            int trueIndex = getTrueIndex(self.start - arr.length + i);
+            this.items[trueIndex] = arr[i];
+        }
+        self.start -= arr.length;
     }
 
     public void addLast(T item) {
+        T[] arr = (T[]) new Object[1];
+        arr[0] = item;
+        this.addLast(arr);
     }
 
     public void addLast(T[] arr) {
+        if (this.size == this.items.length) {
+            this.reSize();
+        }
+        for (i = 0; i < arr.length; i += 1;) {
+            int trueIndex = getTrueIndex(self.stop + i + 1);
+            this.items[trueIndex] = arr[i];
+        }
+        self.stop += arr.length;
     }
 
     public boolean isEmpty() {
+        return this.size == 0;
     }
 
     public int size() {
+        return this.size;
     }
 
     public void printDeque() {
     }
 
     public T removeFirst() {
+        int trueIndex = getTrueIndex(this.start);
+        T first = this.items[trueIndex];
+        this.items[trueIndex] = null;
+        this.size -= 1;
+        this.start += 1;
+        return first;
     }
 
     public T removeLast() {
+        int trueIndex = getTrueIndex(this.stop);
+        T last = this.items[trueIndex];
+        this.items[trueIndex] = null;
+        this.size -= 1;
+        this.stop -= 1;
+        return last;
+    }
+
+    private int getTrueIndex(int index) {
+        int trueIndex = ( this.start + index ) % this.items.length;
+        return trueIndex;
     }
 
     public T get(int index) {
-    }
-
-    public T getRecursive(int index) {
-    }
-
-    public T getRecursiveHelper() {
+        int trueIndex = getTrueIndex(index);
+        return this.items[trueIndex];
     }
 
 }
